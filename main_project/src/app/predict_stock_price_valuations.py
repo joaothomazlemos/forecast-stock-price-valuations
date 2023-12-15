e3e324f4931898f17cbccfdcc1526b68e3cbc924
Joao Thomaz Lemos <joaothomazlemos@gmail.com>
2023-12-15 11:09:39 -0300

    creating the initial recomendation nb, saving data from previousnb

--- main_project/src/app/predict_stock_price_valuations.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.arima_model import ARIMA
-from sklearn.metrics import mean_squared_error
